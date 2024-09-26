9a1ddf5d49e506c7d242db9fe5c8890ece5e9ec7
Youssef-abdelouali <youssefabdelouali225@gmail.com>
2024-09-26 08:06:59 -0400

    model created

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -13,6 +13,7 @@ from django.contrib.auth import login, authenticate
 import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
+from .models import CarMake, CarModel
 # from .populate import initiate
 
 
@@ -75,6 +76,16 @@ def logout_request(request):
     data = {"userName":""}
     return JsonResponse(data)
 
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
+    if(count == 0):
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    return JsonResponse({"CarModels":cars})
 
 # Create a `registration` view to handle sign up request
 # @csrf_exempt
